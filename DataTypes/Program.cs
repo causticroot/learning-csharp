592ab3b5412b3fa067a385d6cd5fb0b25318286b
causticroot <macaulayraro@gmail.com>
2020-11-23 23:31:43 -0300

    Adiciona DataTypes.csproj

---
+++ DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TiposPrimitivos
+    {
+        
+    }
+}
