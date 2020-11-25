9d7f7c22c70bbe2a571e6d561b2fbb55346d94fd
causticroot <macaulayraro@gmail.com>
2020-11-24 22:02:17 -0300

    Adiciona Namespace.csproj

---
+++ Namespace/Program.cs
@@ -0,0 +1,27 @@
+﻿//O namespace System
+using System;
+//E o namespace System.Collections
+using System.Collections;
+//São duas coisas diferentes e independentes, namespaces não podem está uns dentro dos outros
+
+/*
+ * Utilidades do namespace:
+    ¹ Organizar tipos logicamente;
+    ² Diferenciar nomes de tipos que parecem ser conflitantes;
+ */
+
+//Ao criar o namespace loja com a classe FormMenu
+namespace Loja
+{
+    public class FormMenu
+    {
+        
+    }
+}
+//Internamente uma função é criada, exemplo
+// public class Loja.FormMenu
+// {
+
+// }
+
+//O using inclui um assembly no projeto (Cria uma referência)
