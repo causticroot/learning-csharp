d056678cf728631b6edbd20aaae7c0ec9ef521aa
causticroot <macaulayraro@gmail.com>
2020-11-24 22:23:02 -0300

    Adiciona EscapeCharacter.csproj

---
+++ EscapeCharacter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EscapeCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aspas simples
+            char aspasSimples = '\'';
+            
+            //Aspas duplas
+            char aspasDuplas = '\"';
+
+            //Backslash
+            Console.WriteLine("Barra invertida. \\");
+
+            //Nulo
+            Console.WriteLine("\0");
+            
+            //Alerta
+            Console.WriteLine("\a");
+            
+            //Backspace
+            Console.WriteLine("\b");
+
+            //Form feed
+            Console.WriteLine("\f");
+
+            //New line
+            Console.WriteLine("New \nLine");
+
+            //Cariage return
+            Console.WriteLine("\r");
+
+            //Tabulação horizontal(\t) e vertical(\v)
+            
+            
+        }
+    }
+}
