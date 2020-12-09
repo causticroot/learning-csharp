a4c0217478fd5118895c7cf8d5634ae59be83001
caustic <macaulayraro@gmail.com>
2020-12-09 00:59:06 -0300

    Adiciona Console.csproj

---
+++ Console/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Console
+{
+    //A referência de system funciona dentro do namespace
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            string str;
+
+            //Escreve no console sem retorno de carro
+            Console.WriteLine("Digite algo e tecle <Enter>: ");
+            
+            //Lê uma string do console
+            str = Console.ReadLine();
+
+            //Escreve no console sem retorno de carro
+            Console.WriteLine("Digite uma letra e tecle <Enter>: ");
+            
+            //Lê um caractere
+            c = Console.ReadLine();
+
+            //Imprime uma linha em branco
+            Console.WriteLine();
+        }
+    }
+}
