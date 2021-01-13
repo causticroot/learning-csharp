a31c410c39d4eb0b3a6c9a6ac100d0e97f02b057
caustic <macaulayraro@gmail.com>
2021-01-12 22:35:28 -0300

    Adiciona a solução de 'Multiples of 3 or 5'(codewars)

---
+++ CodeWars/Multiples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*
+@ Mutiples_of_3_or_5
+ 
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+*/
+
+public static class kata
+{
+    public static int Solution(int value)
+    {
+        int total;
+        for (int i = 1; i <= value; i++)
+        {
+            if(i % 3 == 0 && i < value || i % 5 == 0 && i < value)
+            {
+                total += i;
+            }
+        }
+        return total;
+    }
+}
