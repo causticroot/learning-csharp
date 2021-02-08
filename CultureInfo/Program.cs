2e734c3cda2b5254562bf310fb55d34a4279d5ce
🚀Caustic <macaulayraro@gmail.com>
2021-02-08 02:04:54 -0300

    Adiciona CultureInfo.csproj

---
+++ CultureInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization; //Responsável
+
+namespace Culture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorErrado = double.Parse(Console.ReadLine());
+            System.Console.WriteLine(valorErrado);
+            
+            double valorCerto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine(valorCerto);
+            
+        }
+    }
+}
