b43f08b8ac84cf5eb5efc4f338d50eb9563c03cf
🚀Caustic <macaulayraro@gmail.com>
2021-02-12 03:08:07 -0300

    Adiciona a solução de 'Hells Kitchen'(codewars)

---
+++ CodeWars/HellsKitchen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System;
+/*
+Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+Rules:
+
+Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A'
+should become '@', Any other vowel should become '*'.
+*/
+namespace HellsKitchen
+{
+    public class Kata
+    {
+        public static string Gordon(string a)
+        {
+            StringBuilder frase = new StringBuilder(a);
+            frase.Replace("a", "@")
+                .Replace("e", "*")
+                .Replace("i", "*")
+                .Replace("o", "*")
+                .Replace("u", "*")
+                .Replace(" ", "!!!! ");
+            string fraseNova = string.Format("{0}!!!!", frase).ToUpper();
+            return fraseNova;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gordon("i am a chef"));
+        }
+    }
+}
