f1aacacbff25e20195b52d63624afaff8327ea70
🚀Caustic <macaulayraro@gmail.com>
2021-02-16 00:06:21 -0300

    Conserta quebra de linha em Program.cs

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -8,7 +8,7 @@ namespace Properties
         {
             Produto produto = new Produto();
             produto.Nome = "Feijão";
-            produto.Preco = 4.99;
+            produto.Preco = 10.00;
             produto.Quantidade = 1;
 
             System.Console.WriteLine("Nome: {0} | Preço: {1} | Quantidade: {2} ", produto.Nome, produto.Preco, produto.Quantidade);
