f25978a136284cc0262be6e496e12a9113dfc2ad
🚀Caustic <macaulayraro@gmail.com>
2021-02-19 05:22:19 -0300

    Adiciona exemplo de Nuallable

---
+++ NullableValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NullableValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double x = null;
+            /*
+            CS0037: Não é possível converter o valor nulo em 'double'
+            porque ele não é um tipo de valor não anulável
+            */
+
+            Nullable<double> x = null;
+            double? y = null;
+            double? z = 5.32;
+            
+            System.Console.WriteLine(x.GetValueOrDefault());
+            System.Console.WriteLine(y.GetValueOrDefault());
+            System.Console.WriteLine(z.GetValueOrDefault());
+
+            System.Console.WriteLine("Tem um valor: {0}", y.HasValue);
+            System.Console.WriteLine("Tem um valor: {0}", z.HasValue);
+        }
+    }
+}
