c8b51049c46fd016778673c91ac9bf595f1219b7
🚀Caustic <macaulayraro@gmail.com>
2021-03-16 19:09:06 -0300

    Adiciona a solução ValidParentheses(codewars)

---
+++ CodeWars/ValidParentheses/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace ValidParentheses
+{
+    public class kata
+    {
+        public static bool ValidParentheses(string input)
+        {
+            var aux = 0;
+            ArrayList arr = new ArrayList(input.ToCharArray()); 
+            
+            
+
+            if ( !String.IsNullOrEmpty(arr.ToString()) )
+            {
+                foreach (var item in arr)
+                {
+                    System.Console.WriteLine(item);
+                    if (item.Equals('('))
+                    {
+                        aux++;
+                    }else if (item.Equals(')'))
+                    {
+                        aux--;
+                    }
+                }    
+            }else
+            {
+                return true;
+            }
+            return (aux == 0);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(ValidParentheses("(( aaa ))"));
+            
+            
+            //System.Console.WriteLine((aux == 0));
+           
+
+            // foreach (var item in arr)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+            // if(inputList.FindLast.Equals(')') && inputList.FindIndex(0).Equals('('))
+            // {
+            //     state = true;
+            // }else
+            // {
+            //     state = false;
+            // }
+
+            // System.Console.WriteLine(state);
+
+        }
+    }
+
+    
+}
