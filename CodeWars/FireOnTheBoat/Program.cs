7989a26513a122da07c7b700cadf2b1a0ed797b9
🚀Caustic <macaulayraro@gmail.com>
2021-03-16 19:08:01 -0300

    Adiciona  a solução FireInTheBoat(codewars)

---
+++ CodeWars/FireOnTheBoat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FireOnTheBoat
+{
+    public class kata
+    {
+        public static string FireFight(string s)
+        {
+            return s.Replace("Fire", "~~").Replace("fire", "~~");
+        }
+        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(FireFight("Boat Rudder Mast Boat Hull Water fire Boat Deck Hull fire Propeller Deck fire"));
+        }
+    }
+}
