196bc295e699b3a178da185315261949147b6b20
🚀Caustic <macaulayraro@gmail.com>
2021-03-16 23:38:53 -0300

    Adiciona exemplo de condicional ternária

---
+++ ConditionalTernary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionalTernary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            //             condição     if true       if false
+            string type = (age >= 18) ? "Cara homi" : "Guri";
+            Console.WriteLine($"{type}");
+        }
+    }
+}
