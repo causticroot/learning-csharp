7995399fc96ea122b92034bcf6ccbc92ddad6c51
🚀Caustic <macaulayraro@gmail.com>
2021-03-17 21:29:59 -0300

    Adiciona exemplo de string functions

---
+++ InterestingStringFunctions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterestingStringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "A stonned DUDE A";
+            
+            //Formatar
+            string fraseLower = frase.ToLower();
+            string fraseUpper = frase.ToUpper();
+            string fraseTrim = frase.Trim();
+
+            //Buscar
+            int fraseAtIndex = frase.IndexOf("DUDE");
+            int fraseAtLastIndex = frase.IndexOf("A");
+
+            //Recortar
+            string subFrase = frase.Substring(2);
+            string subFraseUntil = frase.Substring(2, 7);
+            
+            //Substituir
+            string fraseReplace = frase.Replace("A", "HUE");
+            string fraseReplaceChar = frase.Replace('A', 'X');
+
+            //Null, empty, whiteSpace
+            bool fraseIsNull = String.IsNullOrEmpty(frase);
+            bool fraseIsNullOrWitheSpace = String.IsNullOrWhiteSpace(frase);
+        }
+    }
+}
