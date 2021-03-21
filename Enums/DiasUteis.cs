b4aa653474d1ed830fca60bb4c106d43ca217844
🚀Caustic <macaulayraro@gmail.com>
2021-03-20 22:08:30 -0300

    Adiciona o exemplo de Enums

---
+++ Enums/DiasUteis.cs
@@ -0,0 +1,11 @@
+namespace Enums
+{
+    enum DiasUteis // tipo padrão: int
+    {
+        Segunda, //segunda = 0
+        Terça, //terça = 1
+        Quarta, //quarta = 2 ...
+        Quinta,
+        Sexta
+    }
+}
